68e79836a6b607a76f301b7596e9ae1e0f9f51ce
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 20:10:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   emoticon_hash = load_library(file_path)
-  
+  emoticon
 end
 
 def get_english_meaning(file_path, emoticon)
