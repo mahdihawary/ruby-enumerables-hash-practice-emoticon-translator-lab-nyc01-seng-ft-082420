0c3191fdc10e0badccb1074693f807574f8b1f3f
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 20:07:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,11 +10,16 @@ def load_library(file_path)
     emoticons[key][:english]= value[0]
     emoticons[key][:japanese] = value[1]
   end
+  emoticon_hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  
+  emoticon_hash = load_library(file_path)
+  emoticon_hash.each do |key,value|
+    if emoticon_hash[key][value][:english] == emoticon
+      puts emoticon_hash[key][value][:english]
+    end
 end
 
 def get_english_meaning(file_path, emoticon)
