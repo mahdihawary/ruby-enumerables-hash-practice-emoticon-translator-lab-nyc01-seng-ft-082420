4a379fada8f748f2107ee98586d6207bbbaee93f
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 20:10:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
   emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
-end
+
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
