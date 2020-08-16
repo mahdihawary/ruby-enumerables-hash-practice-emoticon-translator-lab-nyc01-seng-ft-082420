6b029ff2ed06b3b55b236d20fa1cc7f308d7f0e9
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 19:59:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ def load_library(file_path)
   # code goes here
   emoticons = YAML.load_file(file_path)
   emoticon_hash= {}
+  
   emoticons.each do |key, value|
     emoticons[key][:english]= value[0]
     emoticons[key][:japanese] = value[1]
@@ -12,6 +13,7 @@ end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
