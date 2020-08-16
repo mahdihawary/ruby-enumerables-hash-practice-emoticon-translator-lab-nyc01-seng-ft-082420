8a815450b80d2c51eeec46ba06df42dae5ae610f
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-16 20:14:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,19 +16,19 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-   library = load_library(file_path)
-  emoticon = library.keys.find do |key|
-    library[key][:english] == emoticon
+   lib = load_library(file_path)
+  emoticon = lib.keys.find do |key|
+    lib[key][:english] == emoticon
   end
-  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
+  emoticon ? lib[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
 
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
-  library = load_library(file_path)
-  emoticon = library.keys.find do |key|
-    library[key][:japanese] == emoticon
+  lib = load_library(file_path)
+  emoticon = lib.keys.find do |key|
+    lib[key][:japanese] == emoticon
   end
   emoticon ? emoticon : "Sorry, that emoticon was not found"
 end
